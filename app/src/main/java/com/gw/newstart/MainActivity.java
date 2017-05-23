06c8fdede4936fad875586ea33c209d720a067fc
gongwen <1798550470@qq.com>
2017-05-23 15:18:50 +0800

    add butterknife library

--- app/src/main/java/com/gw/newstart/MainActivity.java
+++ app/src/main/java/com/gw/newstart/MainActivity.java
@@ -1,13 +1,21 @@
 package com.gw.newstart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
