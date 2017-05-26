c1d5a96e0dce8d848f25236f997e6fe4f9b5eb42
gongwen <1798550470@qq.com>
2017-05-26 10:29:15 +0800

    update

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 ./gradlew -Pmarket=markets.txt clean apkRelease --stacktrace
 ##### adb 安装apk
 adb install -r ./build/archives/com.gw.newstart-huawei-release-v1.0.0-1.apk
+
+### 参考
+##### [给Android开发者的RxJava详解-扔物线](http://gank.io/post/560e15be2dca930e00da1083)
+##### [手把手教你使用 RxJava 2.0（一）](http://www.jianshu.com/p/d149043d103a)
+##### [手把手教你使用 RxJava 2.0（二）](http://www.jianshu.com/p/310726a75045)
+##### [手把手教你使用 RxJava 2.0（三）](http://www.jianshu.com/p/1f4867ce3c01)
+##### [给初学者的RxJava2.0教程(四)](http://www.jianshu.com/p/bb58571cdb64)
+##### [RxJava 的周末狂欢](https://juejin.im/entry/5695c3ba60b2d6907c9081ef)
+##### [RxJava2.0你不知道的事](http://www.jianshu.com/p/785d9dfb0a5b)
+
+
---
+++ app/src/main/java/com/gw/newstart/rxcompat/SchedulersCompat.java
@@ -0,0 +1,88 @@
+package com.gw.newstart.rxcompat;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by GongWen on 17/5/26.
+ */
+
+public class SchedulersCompat {
+
+    private static final ObservableTransformer computationTransformer = new ObservableTransformer() {
+        @Override
+        public ObservableSource apply(@NonNull Observable upstream) {
+            return upstream.subscribeOn(Schedulers.computation())
+                    .observeOn(AndroidSchedulers.mainThread());
+        }
+    };
+
+    private static final ObservableTransformer ioTransformer = new ObservableTransformer() {
+        @Override
+        public ObservableSource apply(@NonNull Observable upstream) {
+            return upstream.subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread());
+        }
+    };
+    private static final ObservableTransformer newThreadTransformer = new ObservableTransformer() {
+        @Override
+        public ObservableSource apply(@NonNull Observable upstream) {
+            return upstream.subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread());
+        }
+    };
+
+    private static final ObservableTransformer singleTransformer = new ObservableTransformer() {
+        @Override
+        public ObservableSource apply(@NonNull Observable upstream) {
+            return upstream.subscribeOn(Schedulers.single())
+                    .observeOn(AndroidSchedulers.mainThread());
+        }
+    };
+    private static final ObservableTransformer trampolineTransformer = new ObservableTransformer() {
+        @Override
+        public ObservableSource apply(@NonNull Observable upstream) {
+            return upstream.subscribeOn(Schedulers.trampoline())
+                    .observeOn(AndroidSchedulers.mainThread());
+        }
+    };
+    private static final ObservableTransformer executorTransformer = new ObservableTransformer() {
+
+        @Override
+        public ObservableSource apply(@NonNull Observable upstream) {
+            return upstream.subscribeOn(Schedulers.from(ExecutorManager.eventExecutor))
+                    .observeOn(AndroidSchedulers.mainThread());
+        }
+    };
+
+    /**
+     * Don't break the chain: use RxJava's compose() operator
+     */
+    public static <T> ObservableTransformer<T, T> applyComputationSchedulers() {
+        return (ObservableTransformer<T, T>) computationTransformer;
+    }
+
+    public static <T> ObservableTransformer<T, T> applyIoSchedulers() {
+        return (ObservableTransformer<T, T>) ioTransformer;
+    }
+
+    public static <T> ObservableTransformer<T, T> applyNewThreadSchedulers() {
+        return (ObservableTransformer<T, T>) newThreadTransformer;
+    }
+
+    public static <T> ObservableTransformer<T, T> applySingleSchedulers() {
+        return (ObservableTransformer<T, T>) singleTransformer;
+    }
+
+    public static <T> ObservableTransformer<T, T> applyTrampolineSchedulers() {
+        return (ObservableTransformer<T, T>) trampolineTransformer;
+    }
+
+    public static <T> ObservableTransformer<T, T> applyExecutorSchedulers() {
+        return (ObservableTransformer<T, T>) executorTransformer;
+    }
+}
